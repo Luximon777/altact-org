15bedb2737877df1ebbf4424af0c4fb74c452afd
Luximon777 <ck.luximon@gmail.com>
2026-02-13 16:32:14 +0100

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -104,11 +104,11 @@ const HeroSection = () => {
           </div>
         </div>
 
-        {/* Logo - Image avec URL absolue */}
+        {/* Logo - Image */}
         <div className="mb-6 animate-fade-in-up">
           <div className="flex flex-col items-center justify-center">
             <img 
-              src="https://luximon777.github.io/altact-org/logo.png" 
+              src={process.env.PUBLIC_URL + '/logo.png'}
               alt="Alt&Act" 
               className="h-20 md:h-28 lg:h-32 w-auto mx-auto"
             />
